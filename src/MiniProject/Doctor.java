13a89b16c38e7bfe9cbcdab8bfcbbd0b637e7249
sakshi26s <sakshishinde2601@gmail.com>
2026-04-21 13:16:31 +0530

    Create Doctor.java

---
+++ src/MiniProject/Doctor.java
@@ -0,0 +1,13 @@
+package MiniProject;
+
+// --------------------- Doctor Class ---------------------
+class Doctor {
+
+    String name;
+    String specialization;
+
+    Doctor(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
+}
