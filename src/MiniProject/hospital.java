beba597ea44dc822e8aa8c8c25c7c6a5aa881a4a
sakshi26s <sakshishinde2601@gmail.com>
2026-04-21 13:17:28 +0530

    Create hospital.java

---
+++ src/MiniProject/hospital.java
@@ -0,0 +1,16 @@
+package MiniProject;
+
+// --------------------- Hospital Class ---------------------
+class hospital {
+
+    String name;
+    String address;
+    String contactNumber;
+    double lat, lon;
+
+    hospital(String name, String address, String contactNumber) {
+        this.name = name;
+        this.address = address;
+        this.contactNumber = contactNumber;
+    }
+}
